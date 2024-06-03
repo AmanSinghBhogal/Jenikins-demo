12c087d43fd6ca543886247e8483213265c13bfc
Aman.Bhogal-Coditas <aman.bhogal@coditas.com>
2024-06-03 11:47:54 +0530

    Version 2 app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+# This is version 2 for the app
 print("\nHello World \n")
 
-for i in range(5):
-    for j in range(5):
-        if((j==0 or j==4) or (i==0 or i==4)):
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+for c in range(2):
+    for i in range(5):
+        for j in range(5):
+            if((j==0 or j==4) or (i==0 or i==4)):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
